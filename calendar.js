3a5981ebea5a0bba5e639a7b0d6a3d0abff08f78
Ali Hamdami <77199231+alihamdami@users.noreply.github.com>
2025-09-02 21:30:18 +0300

    CALENDAR VIEW INTIAL INTERFACE

--- calendar.js
+++ calendar.js
@@ -14,7 +14,34 @@ document.addEventListener("DOMContentLoaded", function () {
   });
 
   function renderCalendar() {
-    
+    const today = new Date();
+    const year = today.getFullYear();
+    const month = today.getMonth(); //0 index = january
+
+    const firstDay = new Date(year, month, 1).getDay(); 
+    const daysInMonth = new Date(year, month + 1, 0).getDate();
+
+    const calendarBody = document.getElementById("calendar-body");
+    calendarBody.innerHTML = ""; // clear previous calendar
+
+    let day = 1;
+    for (let week = 0; week < 6; week++) {
+      const row = document.createElement("tr");
+      for (let d = 0; d < 7; d++) {
+        const cell = document.createElement("td");
+        if(week === 0 && d < firstDay) {
+          cell.textContent = "";
+        } else if (day > daysInMonth) {
+          cell.textContent = "";
+        } else {
+          cell.textContent = day;
+          // here we add jalali/hijri conversion and special day info
+          day++;
+        }
+        row.appendChild(cell);
+      }
+      calendarBody.appendChild(row);
+    }
   }
 
   renderCalendar();
